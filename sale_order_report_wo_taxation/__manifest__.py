a404005d8607c2f90e1d9737115eb1da0cac7c9b
David Alonso // Solvos <david.alonso@solvos.es>
2021-12-14 18:26:02 +0100

    [ADD] sale_order_report_wo_taxation

---
+++ sale_order_report_wo_taxation/__manifest__.py
@@ -0,0 +1,18 @@
+# © 2021 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Sale Order Report Without Taxation",
+    "summary": """
+        Hides taxes data in sale order report
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "13.0.1.0.0",
+    'category': "Operations/Sale",
+    "website": "https://github.com/solvosci/slv-sale",
+    "depends": ["sale"],
+    "data": [
+        "reports/sale_order_report.xml"
+    ],
+    'installable': True,
+}
