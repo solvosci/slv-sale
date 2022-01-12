bce5b64352fa93bf38ed35f549cad05d4db97a36
ChristianSantamaria <christiancameselle@hotmail.com>
2022-01-12 18:16:13 +0100

    [ADD] sale_order_move_menu

---
+++ sale_order_move_menu/__manifest__.py
@@ -0,0 +1,19 @@
+# © 2022 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Sale Order Move Menu",
+    "summary": """
+        Adds new menu for sale related moves
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "13.0.1.0.0",
+    "category": "Sale",
+    "website": "https://github.com/solvosci/slv-sale",
+    "depends": ["sale_stock"],
+    "data": [
+        "views/stock_move_views.xml",
+        "views/sale_order_views.xml",
+    ],
+    'installable': True,
+}
---
+++ sale_order_move_menu/models/stock_move.py
@@ -0,0 +1,23 @@
+# © 2022 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3.0 (https://www.gnu.org/licenses/lgpl-3.0.html)
+
+from odoo import api, fields, models
+
+
+class StockMove(models.Model):
+    _inherit = "stock.move"
+
+    product_categ_id = fields.Many2one(related='product_id.categ_id')
+    currency_id = fields.Many2one(related='sale_line_id.currency_id')
+    sale_order = fields.Many2one(related='sale_line_id.order_id', store=True)
+    sale_price_unit = fields.Float(related='sale_line_id.price_unit')
+    sale_price_amount_total = fields.Monetary(
+        compute="_compute_sale_price_amount_total",
+        string="Amount Total",
+        store=True,
+    )
+
+    @api.depends("sale_price_unit", "quantity_done")
+    def _compute_sale_price_amount_total(self):
+        for record in self:
+            record.sale_price_amount_total = record.sale_price_unit * record.quantity_done
