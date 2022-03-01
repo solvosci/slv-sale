d45e301bafd8608c088b05d907f78b6aec885bc8
ChristianSantamaria <christiancameselle@hotmail.com>
2022-03-01 09:32:57 +0100

    [ADD] sale_order_line_description_settings

---
+++ sale_order_line_description_settings/__manifest__.py
@@ -0,0 +1,20 @@
+# © 2021 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Sale Order Line Description Settings",
+    "summary": """
+        Add some options for the product description like:
+            - Hide internal reference
+            - Remove line break when you have description sale
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "14.0.1.0.0",
+    "category": "Sale",
+    "website": "https://github.com/solvosci/slv-sale",
+    "depends": ["sale"],
+    "data": [
+        "views/res_config_settings_views.xml",
+    ],
+    'installable': True,
+}
