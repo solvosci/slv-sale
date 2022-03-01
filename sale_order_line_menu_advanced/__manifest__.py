a17ed5628eb072b6ee19b239dee865abcc47d9e6
David Alonso // Solvos <david.alonso@solvos.es>
2022-03-01 14:05:52 +0100

    [ADD] sale_order_line_menu_advanced

---
+++ sale_order_line_menu_advanced/__manifest__.py
@@ -0,0 +1,18 @@
+# © 2022 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Sale Order Line Menu - Advanced additions",
+    "summary": """
+        Adds new fields to sale order line menu
+    """,
+    "author": "Solvos",
+    "license": "AGPL-3",
+    "version": "13.0.1.0.0",
+    "category": "Sale",
+    "website": "https://github.com/solvosci/slv-sale",
+    "depends": ["sale_stock", "sale_order_line_menu"],
+    "data": [
+        "views/sale_order_line_views.xml",
+    ],
+    'installable': True,
+}
