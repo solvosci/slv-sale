9f9d3d1cea39856c6958d6d100aa3966d2f714ce
David Alonso // Solvos <david.alonso@solvos.es>
2022-03-10 13:02:24 +0100

    [IMP] sale_order_move_menu_spil: invoice date placed with other non-move fields

--- sale_order_move_menu_spil/__manifest__.py
+++ sale_order_move_menu_spil/__manifest__.py
@@ -7,7 +7,7 @@
     """,
     "author": "Solvos",
     "license": "AGPL-3",
-    "version": "13.0.1.0.0",
+    "version": "13.0.1.1.0",
     "category": "Sale",
     "website": "https://github.com/solvosci/slv-sale",
     "depends": ["sale_order_move_menu", "stock_picking_invoice_link"],
