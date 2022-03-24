716befefb5ef9978a59c7cf3538eb38a0fba73d3
David Alonso // Solvos <david.alonso@solvos.es>
2022-03-24 13:51:55 +0100

    [ADD] sale_order_report_product_code

---
+++ sale_order_report_product_code/__manifest__.py
@@ -0,0 +1,20 @@
+# © 2022 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Sale Order Report - Add product code",
+    "summary": """
+        Splits Sale Order Report in two, in order to provide a new one with
+        the product code column
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "14.0.1.0.0",
+    "category": "Sales",
+    "website": "https://github.com/solvosci/slv-sale",
+    "depends": ["sale"],
+    "data": [
+        "report/sale_report_templates.xml",
+        "report/sale_report.xml",
+    ],
+    'installable': True,
+}
