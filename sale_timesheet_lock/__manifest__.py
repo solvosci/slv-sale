67e81ba21674c11774330763df934c340d3e9bb6
ChristianSantamaria <christiancameselle@hotmail.com>
2022-04-22 10:21:16 +0200

    [FIX] sale_timesheet_lock: permissions error
    With this fix, users doe't need sale order access when validating
    writing permissions for their own timesheets

--- sale_timesheet_lock/__manifest__.py
+++ sale_timesheet_lock/__manifest__.py
@@ -8,7 +8,7 @@
     """,
     "author": "Solvos",
     "license": "LGPL-3",
-    "version": "14.0.1.0.1",
+    "version": "14.0.1.0.2",
     "category": "Project",
     "website": "https://github.com/solvosci/slv-sale",
     "depends": ["sale_timesheet"],
