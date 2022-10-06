31c8e48ed71b3e49bdc9e3f55a3da43d662fd34d
Christian Santamaria <christian.santamaria@solvos.es>
2022-10-06 08:24:22 +0200

    [ADD] fcd_sale_order

---
+++ fcd_sale_order/__manifest__.py
@@ -0,0 +1,21 @@
+# © 2021 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "FCD Sale Order",
+    "summary": """
+        Adds sale order link with fcd
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "14.0.1.0.0",
+    "category": "Sale",
+    "website": "https://github.com/solvosci/slv-sale",
+    "depends": [
+        "fcd_weight_scale_mrp",
+        "sale_order_lot_selection",
+        "sale_margin",
+        "sale_order_secondary_unit",
+    ],
+    "data": [],
+    'installable': True,
+}
