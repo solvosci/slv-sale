9fda0987130982f180b6c51f910081b71bc99651
Christian Santamaria <christian.santamaria@solvos.es>
2022-10-26 17:09:40 +0200

    [FIX] sale_order_secondary_unit

--- sale_order_secondary_unit/models/sale_order.py
+++ sale_order_secondary_unit/models/sale_order.py
@@ -17,60 +17,60 @@ class SaleOrderLine(models.Model):
         ondelete="restrict",
     )
 
-    @api.onchange("secondary_uom_id", "secondary_uom_qty")
-    def _onchange_secondary_uom(self):
-        if not self.secondary_uom_id:
-            return
-        factor = self.secondary_uom_id.factor * self.product_uom.factor
-        qty = float_round(
-            self.secondary_uom_qty * factor,
-            precision_rounding=self.product_uom.rounding,
-        )
-        if (
-            float_compare(
-                self.product_uom_qty, qty, precision_rounding=self.product_uom.rounding
-            )
-            != 0
-        ):
-            self.product_uom_qty = qty
+    # @api.onchange("secondary_uom_id", "secondary_uom_qty")
+    # def _onchange_secondary_uom(self):
+    #     if not self.secondary_uom_id:
+    #         return
+    #     factor = self.secondary_uom_id.factor * self.product_uom.factor
+    #     qty = float_round(
+    #         self.secondary_uom_qty * factor,
+    #         precision_rounding=self.product_uom.rounding,
+    #     )
+    #     if (
+    #         float_compare(
+    #             self.product_uom_qty, qty, precision_rounding=self.product_uom.rounding
+    #         )
+    #         != 0
+    #     ):
+    #         self.product_uom_qty = qty
 
-    @api.onchange("product_uom_qty")
-    def _onchange_product_uom_qty_sale_order_secondary_unit(self):
-        if not self.secondary_uom_id:
-            return
-        factor = self.secondary_uom_id.factor * self.product_uom.factor
-        qty = float_round(
-            self.product_uom_qty / (factor or 1.0),
-            precision_rounding=self.secondary_uom_id.uom_id.rounding,
-        )
-        if (
-            float_compare(
-                self.secondary_uom_qty,
-                qty,
-                precision_rounding=self.secondary_uom_id.uom_id.rounding,
-            )
-            != 0
-        ):
-            self.secondary_uom_qty = qty
+    # @api.onchange("product_uom_qty")
+    # def _onchange_product_uom_qty_sale_order_secondary_unit(self):
+    #     if not self.secondary_uom_id:
+    #         return
+    #     factor = self.secondary_uom_id.factor * self.product_uom.factor
+    #     qty = float_round(
+    #         self.product_uom_qty / (factor or 1.0),
+    #         precision_rounding=self.secondary_uom_id.uom_id.rounding,
+    #     )
+    #     if (
+    #         float_compare(
+    #             self.secondary_uom_qty,
+    #             qty,
+    #             precision_rounding=self.secondary_uom_id.uom_id.rounding,
+    #         )
+    #         != 0
+    #     ):
+    #         self.secondary_uom_qty = qty
 
-    @api.onchange("product_uom")
-    def _onchange_product_uom_sale_order_secondary_unit(self):
-        if not self.secondary_uom_id:
-            return
-        factor = self.product_uom.factor * self.secondary_uom_id.factor
-        qty = float_round(
-            self.product_uom_qty / (factor or 1.0),
-            precision_rounding=self.product_uom.rounding,
-        )
-        if (
-            float_compare(
-                self.secondary_uom_qty,
-                qty,
-                precision_rounding=self.product_uom.rounding,
-            )
-            != 0
-        ):
-            self.secondary_uom_qty = qty
+    # @api.onchange("product_uom")
+    # def _onchange_product_uom_sale_order_secondary_unit(self):
+    #     if not self.secondary_uom_id:
+    #         return
+    #     factor = self.product_uom.factor * self.secondary_uom_id.factor
+    #     qty = float_round(
+    #         self.product_uom_qty / (factor or 1.0),
+    #         precision_rounding=self.product_uom.rounding,
+    #     )
+    #     if (
+    #         float_compare(
+    #             self.secondary_uom_qty,
+    #             qty,
+    #             precision_rounding=self.product_uom.rounding,
+    #         )
+    #         != 0
+    #     ):
+    #         self.secondary_uom_qty = qty
 
     @api.onchange("product_id")
     def _onchange_product_id_sale_order_secondary_unit(self):
