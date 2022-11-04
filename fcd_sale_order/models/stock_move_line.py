58ca059546803309df2adb47f32bb21a90b4f1a6
David Alonso // Solvos <david.alonso@solvos.es>
2022-11-04 19:01:56 +0100

    [IMP] fcd_sale_order: margin by stock move line - Sales Margin pivot

--- fcd_sale_order/models/stock_move_line.py
+++ fcd_sale_order/models/stock_move_line.py
@@ -7,6 +7,23 @@ from odoo import api, fields, models
 class StockMoveLine(models.Model):
     _inherit = "stock.move.line"
 
+    partner_id = fields.Many2one(
+        related="move_id.partner_id",
+        store=True,
+        string="Partner"
+    )
+    fcd_origin_lot_id = fields.Many2one(
+        related="lot_id.fcd_origin_lot_id",
+        store=True,
+    )
+    sale_date_order = fields.Datetime(
+        related="move_id.sale_line_id.order_id.date_order",
+        store=True,
+    )
+    sale_currency_id = fields.Many2one(
+        related="move_id.sale_line_id.currency_id",
+    )
+
     fcd_purchase_price_kg = fields.Float(
         string="Purchase Price (kg)",
         digits="Product Price",
@@ -19,10 +36,11 @@ class StockMoveLine(models.Model):
         compute="_compute_fcd_price_data_kg",
         store=True,
     )
-    sale_margin = fields.Float(
+    sale_margin = fields.Monetary(
         string="Margin",
         digits="Product Price",
         compute="_compute_fcd_price_data_kg",
+        currency_field="sale_currency_id",
         store=True,
     )
 
@@ -42,7 +60,7 @@ class StockMoveLine(models.Model):
             lambda x: x.product_uom_id.category_id == uom_kg_categ
         )
         for sml in sml_update:
-            lot_id = sml.lot_id.fcd_origin_lot_id or sml.lot_id
+            lot_id = sml.fcd_origin_lot_id or sml.lot_id
             # TODO lot_id.sudo() if needed
             sml.fcd_purchase_price_kg = (
                 lot_id
@@ -51,16 +69,11 @@ class StockMoveLine(models.Model):
             )
             # TODO move_id.sudo() if needed
             sale_line_id = sml.move_id.sale_line_id
-            quantity_kg = (
-                sale_line_id
-                and sale_line_id.product_uom._compute_quantity(sml.qty_done, uom_kg)
-                or 0.0
-            )
             sml.sale_price_kg = (
                 sale_line_id
-                and quantity_kg
-                and sale_line_id.price_unit * (sml.qty_done / quantity_kg)
+                and sale_line_id.price_unit * sale_line_id.product_uom.factor
             )
+            quantity_kg = sml.product_uom_id._compute_quantity(sml.qty_done, uom_kg)
             sml.sale_margin = quantity_kg*(sml.sale_price_kg - sml.fcd_purchase_price_kg)
         # Other UoM stock move lines
         (self - sml_update).write({
