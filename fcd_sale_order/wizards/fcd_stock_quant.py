c978148a747b4b205c632c33c741b8ec4c17f023
Christian Santamaria <christian.santamaria@solvos.es>
2022-12-28 13:14:25 +0100

    [FIX] fcd_sale_order: fix product_uom_qty in imports

--- fcd_sale_order/wizards/fcd_stock_quant.py
+++ fcd_sale_order/wizards/fcd_stock_quant.py
@@ -1,5 +1,5 @@
 # © 2022 Solvos Consultoría Informática (<http://www.solvos.es>)
-# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import models, fields
 
