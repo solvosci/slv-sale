da39b31fd5db6b88152ee2a10987ae09a4b914f4
LuPinheiro <gerallupinheiro@gmail.com>
2023-01-11 14:06:02 +0100

    [ADD] sale_order_shipping_tree

---
+++ sale_order_shipping_tree/__manifest__.py
@@ -0,0 +1,20 @@
+# © 2023 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Sale Order Shipping Tree",
+    "summary": """
+        Sale Order Shipping Tree
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "15.0.1.0.0",
+    "category": "Sale",
+    "website": "https://github.com/solvosci/slv-sale",
+    "depends": [
+        "sale"
+    ],
+    "data": [
+        "views/sale_order.xml"
+    ],
+    'installable': True,
+}
