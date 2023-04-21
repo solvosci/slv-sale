6386285b625915c41fccdaf1c837e61f9c4c9de2
LuPinheiro <gerallupinheiro@gmail.com>
2023-04-21 12:23:57 +0200

    [ADD] sale_order_cmtdate_repos

---
+++ sale_order_cmtdate_repos/__manifest__.py
@@ -0,0 +1,16 @@
+# © 2023 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License AGPL-3 - See https://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Sale Order Commitment Date Reposition",
+    "summary": """
+        Places commitment and expected Sales Order date in order form header.
+    """,
+    "author": "Solvos",
+    "license": "AGPL-3",
+    "version": "15.0.1.0.0",
+    "category": "Sale",
+    "website": "https://github.com/solvosci/slv-sale",
+    "depends": ["sale"],
+    "data": ["views/sale_order_views.xml"],
+    "installable": True,
+}
---
+++ sale_order_cmtdate_repos/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+Places commitment and expected Sales Order date in order form header.
