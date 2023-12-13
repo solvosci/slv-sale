a5d1b63998db432b765f7e16e283da434bdf3834
David Alonso // Solvos <david.alonso@solvos.es>
2023-12-13 12:27:21 +0100

    [ADD] sale_order_date_visible
    Makes order date visible even when initial order creation, which in
    standard is only visible in debug mode.

---
+++ sale_order_date_visible/__manifest__.py
@@ -0,0 +1,15 @@
+# © 2023 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Sale Order - Make order date always visible",
+    "summary": """
+        Makes order date visible even during order creation
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "13.0.1.0.0",
+    "category": "Sale",
+    "website": "https://github.com/solvosci/slv-sale",
+    "depends": ["sale"],
+    "data": ["views/sale_order_views.xml"],
+}
