1b7fe1a16883e1bf06506b326eaf956a9ca1785e
Iria Alonso <iria.alonso@solvos.es>
2024-10-10 09:11:53 +0200

    [MIG] sale_order_client_order_ref_mandatory: Migration to 17.0

--- sale_order_client_order_ref_mandatory/__manifest__.py
+++ sale_order_client_order_ref_mandatory/__manifest__.py
@@ -7,7 +7,7 @@
     """,
     "author": "Solvos",
     "license": "LGPL-3",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "category": "Sale",
     "website": "https://github.com/solvosci/slv-sale",
     "depends": ["sale"],
