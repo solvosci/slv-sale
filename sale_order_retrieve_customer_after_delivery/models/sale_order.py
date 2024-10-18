03a8b4b683921d98479b38249a23ed70a11e2438
David Alonso // Solvos <david.alonso@solvos.es>
2024-10-18 19:04:44 +0200

    [ADD] sale_order_retrieve_customer_after_delivery - workaround needed

--- sale_order_retrieve_customer_after_delivery/models/sale_order.py
+++ sale_order_retrieve_customer_after_delivery/models/sale_order.py
@@ -5,14 +5,22 @@ from odoo import models, api, fields
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    @api.onchange('partner_shipping_id')
-    def _onchange_partner_shipping_id(self):
-        for order in self:
-            order.with_context(skip_change_shipping=True).partner_id = order.partner_shipping_id.commercial_partner_id
+    partner_shipping_2_id = fields.Many2one(
+        comodel_name='res.partner',
+        string="Delivery Address (new)",
+        required=True,
+        check_company=True,
+        # index='btree_not_null',
+    )    
+
+    @api.onchange('partner_shipping_2_id')
+    def _onchange_partner_shipping_2_id(self):
+        self.partner_shipping_id = self.partner_shipping_2_id
+        self.partner_id = self.partner_shipping_2_id.commercial_partner_id
         
-    @api.depends('partner_id')
+    @api.depends("partner_shipping_2_id")
     def _compute_partner_shipping_id(self):
-        if self.env.context.get("skip_change_shipping", False):
-            pass
-        else:
-            super()._compute_partner_shipping_id()
+        # Overridden default behavior:
+        # https://github.com/odoo/odoo/blob/98d9f02f89b9f04c9d2d3bf0ba73c4778d511c4b/addons/sale/models/sale_order.py#L363-L366
+        for order in self:
+            order.partner_shipping_id = order.partner_shipping_2_id
