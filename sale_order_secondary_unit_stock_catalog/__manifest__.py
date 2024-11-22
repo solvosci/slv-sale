9721924eaf1277cf537e886eef6cf5cb76a042a3
David Alonso // Solvos <david.alonso@solvos.es>
2024-11-22 15:23:25 +0100

    [ADD] sale_order_secondary_unit_stock_catalog
    Shows stock in secondary unit within Sales Orders Catalog.
    
    The stock is only shown when is different than zero and the product has
    default sales secondary unit defined in its form.

---
+++ sale_order_secondary_unit_stock_catalog/__manifest__.py
@@ -0,0 +1,15 @@
+# © 2024 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Sale Order Secondary Unit - Catalog - Show Stock in Secondary Unit",
+    "summary": """
+        Shows stock in secondary unit within Sales Orders Catalog
+    """,
+    "author": "Solvos",
+    "license": "AGPL-3",
+    "version": "17.0.1.0.0",
+    "category": "Sales/Sales",
+    "website": "https://github.com/solvosci/slv-sale",
+    "depends": ["sale_order_secondary_unit", "stock"],
+    "data": ["views/product_product_views.xml"],
+}
---
+++ sale_order_secondary_unit_stock_catalog/readme/DESCRIPTION.md
@@ -0,0 +1,4 @@
+Shows stock in secondary unit within Sales Orders Catalog.
+
+The stock is only shown when is different than zero and the product has default
+sales secondary unit defined in its form.
