6788e09960b167f4c2a6206286a61f9ac86547a2
David Alonso // Solvos <david.alonso@solvos.es>
2024-12-21 13:10:25 +0100

    [ADD] sale_order_secondary_unit_default
    For special case when a sales line is initialized, value 1.0 is set for main
    product qty.
    
    In that case, if secondary unit is defined, we set such value for secondary
    unit instead, and default value = 1.0 for secondary unit is achieved.

---
+++ sale_order_secondary_unit_default/__manifest__.py
@@ -0,0 +1,15 @@
+# © 2024 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Sale Order Secondary Unit - Default value",
+    "summary": """
+        Forces 1.0 as default value for secondary unit
+        when a sale line is created
+    """,
+    "author": "Solvos",
+    "license": "AGPL-3",
+    "version": "17.0.1.0.0",
+    "category": "Sales/Sales",
+    "website": "https://github.com/solvosci/slv-sale",
+    "depends": ["sale_order_secondary_unit"],
+}
