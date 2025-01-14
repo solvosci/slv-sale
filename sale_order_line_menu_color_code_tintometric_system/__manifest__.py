b901b1b5a0f524406b45d83bf4e40f25137a6239
Iria Alonso <iria.alonso@solvos.es>
2025-01-14 10:47:56 +0100

    [ADD] sale_order_line_menu_color_code_tintometric_system

---
+++ sale_order_line_menu_color_code_tintometric_system/__manifest__.py
@@ -0,0 +1,20 @@
+# © 2024 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Sale Order Line Menu Color Code Tintometric System",
+    "summary": """
+        Add the column 'color_code_tintometric' in the menu of the order lines and group and search by it.
+    """,
+    "author": "Solvos",
+    "license": "AGPL-3",
+    "version": "17.0.1.0.0",
+    "category": "Sales/Sales",
+    "website": "https://github.com/solvosci/slv-sale",
+    "depends": [
+        "sale_order_line_menu"
+    ],
+    "data": [
+        "views/sale_order_line_views.xml"
+    ],
+    'installable': True,
+}
---
+++ sale_order_line_menu_color_code_tintometric_system/readme/DESCRIPTION.md
@@ -0,0 +1,1 @@
+Add the column 'color_code_tintometric' in the menu of the order lines and group and search by it.
