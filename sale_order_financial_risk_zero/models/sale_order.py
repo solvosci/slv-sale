7965585802ff8d1c9f76de23a6ad002ec17bec61
Iria Alonso <iria.alonso@solvos.es>
2025-04-22 15:54:57 +0200

    [ADD] sale_order_financial_risk_zero By default, if risk limit on sales orders of a customer is exactly zero, all sales orders will be blocked, except if the order is exactly zero. Now if that condition is true, it also blocks the order.

---
+++ sale_order_financial_risk_zero/models/sale_order.py
@@ -0,0 +1,27 @@
+# © 2025 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License AGPL-3.0 (https://www.gnu.org/licenses/agpl-3.0.html)
+
+from odoo import models, _, fields
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    def evaluate_risk_message(self, partner):
+        res = super().evaluate_risk_message(partner)
+        if res:
+            return res
+
+        risk_amount = self.currency_id._convert(
+            self.amount_total,
+            self.company_id.currency_id,
+            self.company_id,
+            self.date_order and self.date_order.date() or fields.Date.context_today(self),
+            round=False,
+        )
+
+        if partner.risk_sale_order_include and (
+            (partner.risk_total + risk_amount) >= partner.credit_limit
+        ):
+            return _("This sale order exceeds the financial risk.\n")
+        else:
+            return ""
---
+++ sale_order_financial_risk_zero/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+By default, if risk limit on sales orders of a customer is exactly zero,
+all sales orders will be blocked, except if the order is exactly zero.
+Now if that condition is true, it also blocks the order.
