6075d5307215c66b8d435d3cf9e81204109942c6
Iria Alonso <iria.alonso@solvos.es>
2025-04-22 15:54:57 +0200

    [ADD] sale_order_financial_risk_zero By default, if risk limit on sales orders of a customer is exactly zero, all sales orders will be blocked, except if the order is exactly zero. Now if that condition is true, it also blocks the order.

---
+++ sale_order_financial_risk_zero/__manifest__.py
@@ -0,0 +1,17 @@
+# © 2025 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Sale Order Financial Risk Zero",
+    "summary": """
+        By default, if risk limit on sales orders of a customer is exactly zero,
+        all sales orders will be blocked, except if the order is exactly zero.
+        Now if that condition is true, it also blocks the order.
+    """,
+    "author": "Solvos",
+    "license": "AGPL-3",
+    "version": "13.0.1.0.0",
+    "category": "Sale",
+    "website": "https://github.com/solvosci/slv-sale",
+    "depends": ["sale_financial_risk"],
+    'installable': True,
+}
