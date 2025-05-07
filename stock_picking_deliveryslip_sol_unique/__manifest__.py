b8525b080109e925efa0c19d711a9b17979bddbe
Iria Alonso <iria.alonso@solvos.es>
2025-05-07 17:21:26 +0200

    [ADD] stock_picking_deliveryslip_sol_unique By default, when some moves are exactly the same on a picking, they are merged. Merging is now prevented only on picking from sale orders.

---
+++ stock_picking_deliveryslip_sol_unique/__manifest__.py
@@ -0,0 +1,18 @@
+# © 2025 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Stock Picking Deliveryslip Sale Order Line Unique",
+    "summary": """
+        By default, when some moves are exactly the same on a picking, they are merged.
+        Merging is now prevented only on picking from sale orders.
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "17.0.1.0.0",
+    "category": "Inventory/Stock",
+    "website": "https://github.com/solvosci/slv-sale",
+    "depends": [
+        "sale_stock",
+    ],
+    "installable": True,
+}
---
+++ stock_picking_deliveryslip_sol_unique/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+By default, when some moves are exactly the same on a picking, they are merged.
+Merging is now prevented only on picking from sale orders.
