3ff2b2d3da673fd5a88f6ba51fecfdda15337e70
Iria Alonso <iria.alonso@solvos.es>
2025-07-28 11:05:49 +0200

    [ADD] sale_order_pending_move_menu_pb Link ‘sale_order_pending_move_menu’ and ‘product_brand’ to add the 'brand' field to the 'pending sales moves' view

---
+++ sale_order_pending_move_menu_pb/__manifest__.py
@@ -0,0 +1,18 @@
+# © 2025 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Sale Order Pending Move Menu Link Product Brand",
+    "summary": """
+        Link ‘sale_order_pending_move_menu’ and ‘product_brand’ to add the 'brand' field to the 'pending sales moves' view
+    """,
+    "author": "Solvos",
+    "license": "AGPL-3",
+    "version": "17.0.1.0.0",
+    'category': "Operations/Sale",
+    "website": "https://github.com/solvosci/slv-sale",
+    "depends": ["product_brand", "sale_order_pending_move_menu"],
+    "data": [
+        "views/stock_move_views.xml",
+    ],
+    'installable': True,
+}
