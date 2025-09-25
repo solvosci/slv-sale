2c647754498f38619cf7ebba4afc1775fca8894d
Iria Alonso <iria.alonso@solvos.es>
2025-09-25 14:51:08 +0200

    [ADD] sale_order_invoice_address_security Invoice address is only visible to account users when the addresses are visibles

---
+++ sale_order_invoice_address_security/__manifest__.py
@@ -0,0 +1,18 @@
+# © 2025 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Sale Order Invoice Address Security",
+    "summary": """
+        Invoice address is only visible to account users when the addresses are visibles
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "17.0.1.0.0",
+    "category": "Sale",
+    "website": "https://github.com/solvosci/slv-sale",
+    "depends": ["sale"],
+    "data": [
+        "views/sale_order_views.xml",
+    ],
+    'installable': True,
+}
