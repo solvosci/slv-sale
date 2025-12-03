db59aa0c068ebded44eac975469886ecc3192b44
Iria Alonso <iria.alonso@solvos.es>
2025-12-03 12:13:31 +0100

    [UPD] sale_order_partner_available_delivery: undo the billing address as the customer's address

--- sale_order_partner_available_delivery/__manifest__.py
+++ sale_order_partner_available_delivery/__manifest__.py
@@ -8,7 +8,7 @@
     """,
     "author": "Solvos",
     "license": "LGPL-3",
-    "version": "17.0.1.0.0",
+    "version": "17.0.1.1.0",
     "category": "Inventory/Purchase",
     "website": "https://github.com/solvosci/slv-sale",
     "depends": [
