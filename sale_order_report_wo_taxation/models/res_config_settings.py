91a4cda0fb2ddea4205614dcc4f91eb94ed9cc4d
Iria Alonso <iria.alonso@solvos.es>
2025-12-11 08:08:14 +0100

    [MIG] sale_order_report_wo_taxation: Migration to 18.0

--- sale_order_report_wo_taxation/models/res_config_settings.py
+++ sale_order_report_wo_taxation/models/res_config_settings.py
@@ -1,6 +1,8 @@
 # © 2024 Solvos Consultoría Informática (<http://www.solvos.es>)
 # License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+
 from odoo import models, fields
+
 
 class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
