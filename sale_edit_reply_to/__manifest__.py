58216cea6a4519e434472cf3b6968df3a61f1770
cgarcia-solvos <carlos.garcia@solvos.es>
2026-02-06 11:14:37 +0100

    [ADD] sale_edit_reply_to
    Allow setting a fixed Reply-To address for Sale Order email notifications via the system parameter.

---
+++ sale_edit_reply_to/__manifest__.py
@@ -0,0 +1,17 @@
+# © 2021 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Sale Edit Reply To",
+    "summary": """
+        Allow setting a fixed Reply-To address for Sale Order email notifications via the system parameter.
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "18.0.1.0.0",
+    'category': "Tools",
+    "website": "https://github.com/solvosci/",
+    "depends": [
+        'sale',
+    ],
+    'installable': True,
+}
---
+++ sale_edit_reply_to/readme/DESCRIPTION.md
@@ -0,0 +1,1 @@
+Allow setting a fixed Reply-To address for Sale Order email notifications via the system parameter purchase.reply_to_email. If not set, Odoo's default Reply-To is used.
